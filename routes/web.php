25d1ba70fda5499ea7e4959ed554e41134cfe769
Syifa Revalina Kamila <cipakamila19@gmail.com>
2025-02-21 19:35:25 +0700

    percobaan 1

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,29 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/hello', function(){
+Route::get('/hello', function () {
     return 'Hello World';
 });
+
+Route::get('/world', function () {
+    return 'World';
+});
+
+Route::get('/user/{name}', function($name){
+    return 'Nama Saya '.$name;
+});
+
+Route::get('/posts/{post}/comments/{comment}', function 
+($postId, $commentId) {
+return 'Pos ke-'.$postId." Komentar ke-: ".$commentId;
+});
+
+Route::get('/user/{name?}', function ($name=null) {
+    return 'Nama saya '.$name;
+});
+
+Route::get('/user/{name?}', function ($name='Syifa') {
+    return 'Nama saya '.$name;
+    });
+
+       
