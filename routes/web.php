14b35d8a65bc82d33e21bf52bb8e892566fdbc14
Syifa Revalina Kamila <cipakamila19@gmail.com>
2025-02-25 09:39:11 +0700

    commit baru

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,13 @@
 <?php
 
-use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AboutController;
+use App\Http\Controllers\ArticleController;
+use App\Http\Controllers\HomeController;
+use Illuminate\Support\Facades\Route; //mengimpor kelas Route yang digunakan untuk mendefinisikan routes.
+use App\Http\Controllers\ItemController; //mengimpor controller ItemController yang akan menangani request yang terkait dengan "item".
+use App\Http\Controllers\WelcomeController;
+use App\Http\Controllers\PageController;
+use App\Http\Controllers\PhotoController;
 
 /*
 |--------------------------------------------------------------------------
@@ -13,29 +20,53 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/hello', function () {
-    return 'Hello World';
+Route::get('/', function () { //mendefinisikan route untuk path (jalur) /. 
+    return view('welcome'); //mengembalikan view bernama welcome.
 });
 
-Route::get('/world', function () {
+
+Route::get('/hello', [WelcomeController::class, 'hello']);
+
+Route::get('/world', function () { 
     return 'World';
 });
 
-Route::get('/user/{name}', function($name){
-    return 'Nama Saya '.$name;
+Route::get('/sd', function() {
+    return 'Selamat Datang';
 });
 
-Route::get('/posts/{post}/comments/{comment}', function 
-($postId, $commentId) {
-return 'Pos ke-'.$postId." Komentar ke-: ".$commentId;
-});
+// Route::get('/about', function() {
+//     return 'Nama : Adinda Ivanka Maysanda Putri, 
+//     NIM : 2341760058';
+// });
 
-Route::get('/user/{name?}', function ($name=null) {
+Route::get('/user/{name}', function ($name) { 
     return 'Nama saya '.$name;
 });
 
-Route::get('/user/{name?}', function ($name='Syifa') {
-    return 'Nama saya '.$name;
-    });
+Route::get('/posts/{post}/comments/{comment}', function ($postId, $commentId) {
+    return 'Pos ke-'.$postId." Komentar ke-: ".$commentId;
+});
+    
+// Route::get('/articles/{Id}', function($Id) {
+//     return 'Halaman Artikel dengan ID : '. $Id;
+// });
 
-       
+Route::get('/user/{name?}', function ($name='John') { 
+    return 'Nama saya '.$name;
+});
+
+Route::resource('photos', PhotoController::class);
+
+Route::resource('photos', PhotoController::class)->only([
+    'index', 'show'
+]);
+
+Route::resource('photos', PhotoController::class)->except([
+    'create', 'store', 'update', 'destroy'
+]);
+
+Route::get('/greeting', [
+    WelcomeController::class,
+    'greeting'
+]);
